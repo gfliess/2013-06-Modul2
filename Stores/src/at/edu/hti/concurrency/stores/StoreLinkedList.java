b03b64493c1309c58aa74d81364c21a2e7ae9cd9
gsc <gsc@L-Schmitt2.peemdomain.at>
2013-06-11 10:06:00 +0200

    spanni, schmitt stores

---
+++ Stores/src/at/edu/hti/concurrency/stores/StoreLinkedList.java
@@ -0,0 +1,42 @@
+package at.edu.hti.concurrency.stores;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import at.edu.hti.concurrency.Store;
+
+public class StoreLinkedList implements Store {
+
+	List<String> list;
+
+	@Override
+	public String getName() {
+		return "StoreLinkedList";
+	}
+
+	@Override
+	public void initMaxSize(int maxSize) {
+		list = new LinkedList<String>();
+	}
+
+	@Override
+	public void addFirst(String data) {
+		list.add(0, data);
+	}
+
+	@Override
+	public String removeLast() {
+		return list.remove(list.size() - 1);
+	}
+
+	@Override
+	public String removeItem(int index) {
+		return list.remove(index);
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+}
